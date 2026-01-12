4f71d85ebf536432c1684d2e31787a9239de74f9
Adam Hussain <adam@hussmedia.io>
2026-01-11 18:33:29 -0800

    css

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ declare module '@remix-run/server-runtime' {
 
 export default defineConfig({
   server: {
-    allowedHosts: ['telecharger-completely-bright-allied.tryhydrogen.dev'],
+    allowedHosts: ['grew-mile-threatening-stress.tryhydrogen.dev'],
   },
   plugins: [
     tailwindcss(),
