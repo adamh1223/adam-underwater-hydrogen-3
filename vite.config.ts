a940be2d264d17b5887050dcb45b0fd588d3260d
Adam Hussain <adam@hussmedia.io>
2026-01-23 23:44:50 -0800

    inthebox

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ declare module '@remix-run/server-runtime' {
 
 export default defineConfig({
   server: {
-    allowedHosts: ['increased-incoming-martial-bye.tryhydrogen.dev'],
+    allowedHosts: ['businesses-losing-favourites-self.tryhydrogen.dev'],
   },
   plugins: [
     tailwindcss(),
