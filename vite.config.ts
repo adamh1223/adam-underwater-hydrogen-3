3d47fbfb978539dca9223c168eb04008f39fca44
Adam Hussain <adam@hussmedia.io>
2026-01-24 16:05:41 -0800

    review css

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ declare module '@remix-run/server-runtime' {
 
 export default defineConfig({
   server: {
-    allowedHosts: ['businesses-losing-favourites-self.tryhydrogen.dev'],
+    allowedHosts: ['mental-half-potter-blink.tryhydrogen.dev'],
   },
   plugins: [
     tailwindcss(),
